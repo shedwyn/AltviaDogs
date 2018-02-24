ae5416c3256808a24e052baa9a9edd2d2b6b0eb3
Ernie Fough <shedwyn@gmail.com>
2018-02-24 14:28:37 -0700

    oops, started off wrong foot

--- AltviaDogs/settings.py
+++ AltviaDogs/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from . import secrets
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '57%-@(ark962cfbutas(do&y465kpuzi#2y1qc91kx6)yxwer+'
+SECRET_KEY = secrets.secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
