473856ee72bc42560a8d0dbb081f107f72dc58ee
Ernie Fough <shedwyn@gmail.com>
2018-02-24 15:05:47 -0700

    beginning model

--- AltviaDogs/models.py
+++ AltviaDogs/models.py
@@ -1,1 +1,52 @@
+"""Objects/Models/Classes and all that makes those up"""
+
+# from django.contrib.auth.models import User
 from django.db import models
+
+
+class Dog(models.Model):
+    """Dog Descriptors"""
+
+    name = models.CharField(max_length=15)
+    owner = models.ForeignKey('Owner')
+
+    def __str__(self):
+        """docstring"""
+        return 'Dog_str({}, {}, {})'.format(self.id, self.name, self.owner)
+
+    def __repr__(self):
+        """docstring"""
+        return 'Dog_repr({!r}, {!r}, {!r})'.format(
+            self.id, self.name, self.owner)
+
+
+class Owner(models.Model):
+    """Owner descriptors, will supplant with User eventually"""
+
+    name = models.CharField(max_length=40)
+    email = models.EmailField()
+    slack = models.CharField()
+
+    def __str__(self):
+        """docstring"""
+        return 'Owner_str({}, {}, {})', format(
+            self.id, self.name, self.email, self.slack)
+
+    def __repr__(self):
+        """docstring"""
+        return 'Owner_repr({!r}, {!r}, {!r})'.format(
+            self.id, self.name, self.email, self.slack)
+
+
+class RecordFile(models.Model):
+    """Record of Shout-out or 'oopsie'
+
+    links to Dog and Owner
+    """
+
+    REC_TYPE_CHOICES = (
+        ('SO', 'Shout-Out'), ('O', 'Oopsie')
+    )
+
+    title = models.CharField(max_length=50)
+    rec_type = models.Charfield(choices=REC_TYPE_CHOICES)
