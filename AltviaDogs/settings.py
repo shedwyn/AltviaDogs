20498f73f342f36ebc07ae1dc62eaf1f7769cda0
Ernie Fough <shedwyn@gmail.com>
2018-06-07 10:57:41 -0600

    changed to PostGres and added Python pkg

--- AltviaDogs/settings.py
+++ AltviaDogs/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'AltviaDogs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'AltviaDogs',
+        'USER': 'efough',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
