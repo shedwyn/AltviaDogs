861ef95c8999760f99f3af10ab8fed08006b498b
Ernie Fough <shedwyn@gmail.com>
2018-06-25 13:15:07 -0600

    still stubbing and testing logic

--- AltviaDogs/logic.py
+++ AltviaDogs/logic.py
@@ -1,15 +1,32 @@
 """All processing functions for rendering views"""
 
-from datetime import datetime
+from datetime import date
+from calendar import weekday
 from AltviaDogs.models import Dog
 from AltviaDogs.models import Owner
 from AltviaDogs.models import DogDay
 
+list_of_weekday_values = {'0':'Monday', '1':'Tuesday', '2':'Wednesday', '3':'Thursday', '4':'Friday', '5':'Saturday', '6':'Sunday'}
 
-def select_and_test_date(todays_date):
-    date = todays_date
+
+def determine_weekday(year, month, day):
+    weekday = weekday(year, month, day)
+    return list_of_weekday_values[weekday]
+
+
+def select_and_test_date(date):
+    if date = 'today':
+        year = date.today().year
+        month = date.today().month
+        day = date.today().day
+    else:
+        year = '2018'
+        month = '6'
+        day = '22'
+    weekday = determine_weekday(year, month, day)
     # need to find date and Days
     # then test for Sat or sun
+    calendar.weekday(date.today().year, date.today().month, date.today().day)
     current_date = 'date corrected'
     return current_date
 
--- AltviaDogs/views.py
+++ AltviaDogs/views.py
@@ -12,11 +12,12 @@ from django.shortcuts import render
 from . import logic
 # from . import settings
 from .models import Dog
+from datetime import date
 
 
 def render_index_page(request):
     """render home/menu page with all base data"""
-    date = logic.select_and_test_date
+    date = logic.select_and_test_date('today')
     dogs = logic.grab_list_of_dogs
     page_fill = {'date': date, 'dogs': dogs}
     return render(request, 'AltviaDogs/index.html', page_fill)
