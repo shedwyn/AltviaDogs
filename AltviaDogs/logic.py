6ab886e820e6c2961fa61e74d251ed3b066cbe65
Ernie Fough <shedwyn@gmail.com>
2018-06-25 12:09:39 -0600

    stubbing for index page

--- AltviaDogs/logic.py
+++ AltviaDogs/logic.py
@@ -4,3 +4,21 @@ from datetime import datetime
 from AltviaDogs.models import Dog
 from AltviaDogs.models import Owner
 from AltviaDogs.models import DogDay
+
+
+def select_and_test_date(todays_date):
+    date = todays_date
+    # need to find date and Days
+    # then test for Sat or sun
+    current_date = 'date corrected'
+    return current_date
+
+
+def grab_list_of_dogs(select_and_test_date()):
+    date = select_and_test_date()
+    # test to see if record exists for this date
+    # if not - create date
+    dogs = Dog.objects.filter(date)
+    # probably need test for if no dogs then return 'no dogs'
+    # create sort order within logic (assign dog 1, dog 2, dog 3 for layout?)
+    return dogs
--- AltviaDogs/views.py
+++ AltviaDogs/views.py
@@ -9,20 +9,22 @@ from django.http import HttpResponse
 # from django.http import HttpResponseRedirect
 # from django.http import JsonResponse
 from django.shortcuts import render
-# from . import logic
+from . import logic
 # from . import settings
 from .models import Dog
 
 
 def render_index_page(request):
     """render home/menu page with all base data"""
-    page_fill = {}
-    return render(request, 'AltviaDogs/index.html', page_fill)
+    date = logic.select_and_test_date
+    dogs = logic.grab_list_of_dogs
+    page_fill = {'date': date, 'dogs': dogs}
+    return render(request, 'AltviaDogs/index.html', page_fill
 
 
-def index(request):
-    list_of_dogs = Dog.objects.order_by('name')
-    statement = 'Altvia Dogs Temporary Index Page, here are the dogs:'
-    dog_string = ', '.join([d.name for d in list_of_dogs])
-    response_output = statement + '\n\n\n\n\n\n\n' + dog_string
-    return HttpResponse(response_output)
+# def index(request):
+    # list_of_dogs=Dog.objects.order_by('name')
+    # statement='Altvia Dogs Temporary Index Page, here are the dogs:'
+    # dog_string=', '.join([d.name for d in list_of_dogs])
+    # response_output=statement + '\n\n\n\n\n\n\n' + dog_string
+    # return HttpResponse(response_output)
