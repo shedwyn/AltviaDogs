60c8ef6406468c280b2267d994878f8bef2200f0
Ernie Fough <shedwyn@gmail.com>
2018-07-04 12:33:27 -0600

    removed unneeded import

--- AltviaDogs/urls.py
+++ AltviaDogs/urls.py
@@ -19,7 +19,6 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
-# from django.urls import include
 from . import views
 
 urlpatterns = [
