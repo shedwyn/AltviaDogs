20065af923484a4c2da3aae7dda338c04ac5010a
Ernie Fough <shedwyn@gmail.com>
2018-07-12 14:06:41 -0600

    continue to chg settings to prep for Heroku

--- AltviaDogs/settings.py
+++ AltviaDogs/settings.py
@@ -17,6 +17,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 from . import secrets
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -30,9 +31,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = secrets.secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
@@ -131,3 +133,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
