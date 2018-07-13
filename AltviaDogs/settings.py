5d58245fba2df5a33c232f88d977de3e0094efea
Ernie Fough <shedwyn@gmail.com>
2018-07-13 12:57:34 -0600

    changed static to PROJECT ROOT

--- AltviaDogs/settings.py
+++ AltviaDogs/settings.py
@@ -137,13 +137,13 @@ USE_TZ = True
 # PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 # Confirm if needs to change when static hosted in AWS
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'),)
 
 # Heroku database notes per DjangoGirls
 
