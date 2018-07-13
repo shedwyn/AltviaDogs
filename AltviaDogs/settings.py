5abcc7c989f26f502c69e1ff5cad91a5ab86df14
Ernie Fough <shedwyn@gmail.com>
2018-07-13 13:25:18 -0600

    style edits

--- AltviaDogs/settings.py
+++ AltviaDogs/settings.py
@@ -135,7 +135,6 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
