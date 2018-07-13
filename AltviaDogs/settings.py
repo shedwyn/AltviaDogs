e7568ac94c9ac6e6141c37e012df7b9618e8d672
Ernie Fough <shedwyn@gmail.com>
2018-07-13 12:41:17 -0600

    more debugging

--- AltviaDogs/settings.py
+++ AltviaDogs/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
