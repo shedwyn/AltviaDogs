89b43cf8dae5c7bb8041f9854099f48fa942bbbb
Ernie Fough <shedwyn@gmail.com>
2018-07-19 13:37:08 -0600

    added back in decouple for variables

--- AltviaDogs/settings.py
+++ AltviaDogs/settings.py
@@ -18,7 +18,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import dj_database_url
-from decouple import config, Csv
+from decouple import config
+# from decouple import Csv - can add Csv at end of above as well
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
