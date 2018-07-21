62d53170db6753c2b64cd0912ef41d978964ac33
Ernie Fough <shedwyn@gmail.com>
2018-07-21 14:15:02 -0600

    stubbing js for index to transform and validate date entry

---
+++ AltviaDogs/static/index_js.js
@@ -0,0 +1,11 @@
+// js specific to index Page
+
+document.getElementById('date_choice').addEventListener('change', function(){
+  console.log('true');
+})
+
+var checkDateEntryFormat(){}
+
+var registerValidationSequence(){
+  var dateChoice = document.getElementById('date_choice').value
+}
