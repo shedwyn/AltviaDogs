36d4aaefb74af22ed8791f1af867db4a0d0df2f9
Ernie Fough <shedwyn@gmail.com>
2018-08-06 12:42:11 -0600

    correcting some of the js

--- AltviaDogs/templates/AltviaDogs/index_js.js
+++ AltviaDogs/templates/AltviaDogs/index_js.js
@@ -65,11 +65,21 @@ function registerGlobalEventHandlers() {
   // const sourceForm = document.getElementByID('date-choice-form');
   // const dateChoice = document.getElementByID('date_choice').value;
   console.log("starting");
+  // .on - I want it to eval just before submitting and return to the same
+  // state as when person hit "submit" if there was an error, and have an
+  // error alert, but keep the data in the fields as/is
   $("#date-choice-form").on("change", function(){
-    const dateChoice = retrieveDateChoiceValue;
-    console.log(checkForCorrectDateFormat(dateChoice));
-    console.log("date was checked")
+    let dateChoice = retrieveDateChoiceValue;
+    checkForCorrectDateFormat(dateChoice);
+    console.log("date was checked");
   });
+  //$(#date-choice-form).on("submit", function(){
+  //  let dateChoice = retrieveDateChoiceValue;
+  //  checkForCorrectDateFormat(dateChoice);
+  //  formatDateForPost(dateChoice);
+  //  
+
+  //})
     //event.preventDefault();
   };
 
